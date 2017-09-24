1f42f451aa54b25d3893ce407e2ec7d112232374
rcalxrc08 <nicola.scaramuzzino@yandex.com>
2017-09-24 16:30:30 +0200

    added business day test

--- ql/time/calendars/all.hpp
+++ ql/time/calendars/all.hpp
@@ -8,6 +8,7 @@
 #include <ql/time/calendars/jointcalendar.hpp>
 #include <ql/time/calendars/nullcalendar.hpp>
 #include <ql/time/calendars/russia.hpp>
+#include <ql/time/calendars/southafrica.hpp>
 #include <ql/time/calendars/southkorea.hpp>
 #include <ql/time/calendars/target.hpp>
 #include <ql/time/calendars/unitedkingdom.hpp>
