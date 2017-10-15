e0f5aaed3b651a2f5bf6859a05aa9adcc1256c4a
rcalxrc08 <nicola.scaramuzzino@yandex.com>
2017-10-15 18:09:14 +0200

    added all solvers

--- ql/math/solvers1d/all.hpp
+++ ql/math/solvers1d/all.hpp
@@ -1,5 +1,9 @@
 #include <ql/math/solvers1d/bisection.hpp>
 #include <ql/math/solvers1d/brent.hpp>
+#include <ql/math/solvers1d/falseposition.hpp>
+#include <ql/math/solvers1d/finitedifferencenewtonsafe.hpp>
 #include <ql/math/solvers1d/newton.hpp>
 #include <ql/math/solvers1d/newtonsafe.hpp>
+#include <ql/math/solvers1d/ridder.hpp>
+#include <ql/math/solvers1d/secant.hpp>
 
